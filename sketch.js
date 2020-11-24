70ddc6b22fea96f470e9c70516dd747febc8800a
ksycip <56284512+ksycip@users.noreply.github.com>
2020-11-24 00:38:12 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+let video;
+let poseNet;
+
+function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+}
+
+function modelLoaded(){
+  console.log('poseNet ready');
+}
+
+function draw() {
+  image(video, 0, 0);
+  
+}
